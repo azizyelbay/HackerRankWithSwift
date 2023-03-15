72ce9777022cfe6e4c525b1455ee96e9b260893e
azizyelbay <34959497+azizyelbay@users.noreply.github.com>
2023-03-15 22:32:38 +0300

    BillDivision

---
+++ BillDivision.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
+
+func bonAppetit(bill: [Int], k: Int, b: Int) -> Void {
+    var sum: Int = 0
+    for i in 0..<bill.count {
+        if i == k {
+            continue
+        }
+        sum = sum + bill[i]
+    }
+    if (b - sum/2) == 0 {
+        print("Bon Appetit")
+    }
+    else {
+        print((b - sum/2))
+    }
+
+}
+
+guard let firstMultipleInputTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+let firstMultipleInput = firstMultipleInputTemp.split(separator: " ").map{ String($0) }
+
+guard let n = Int(firstMultipleInput[0])
+else { fatalError("Bad input") }
+
+guard let k = Int(firstMultipleInput[1])
+else { fatalError("Bad input") }
+
+guard let billTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let bill: [Int] = billTemp.split(separator: " ").map {
+    if let billItem = Int($0) {
+        return billItem
+    } else { fatalError("Bad input") }
+}
+
+guard bill.count == n else { fatalError("Bad input") }
+
+guard let b = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+bonAppetit(bill: bill, k: k, b: b)
